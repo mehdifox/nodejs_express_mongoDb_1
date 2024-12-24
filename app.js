912b39c473bfdc36bb7349e26b111a558d1821fe
mehdi zouihar <elmehdi.zouihar@gmail.com>
2024-12-24 17:58:41 +0100

    add connection

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+
+mongoose
+    .connect('mongodb+srv://ayoubramic50:3SeP86bFnODHfWnv@mongodb.e0oau.mongodb.net/?retryWrites=true&w=majority&appName=mongodb')
+    .then(() => { 
+        app.listen(port, () => {
+            console.log(`http://localhost:${port}`)
+        })
+     })
+    .catch((err) => { console.log(err) })
 
 app.get('/', (req,res) => {
     res.sendFile("./views/home.html", {root: __dirname})
-})
-
-app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
 })
